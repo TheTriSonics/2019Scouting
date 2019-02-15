89ff16511757717caa3a1c71230808c0f0cebc68
Stale-Bread <35582171+Stale-Bread@users.noreply.github.com>
2019-02-14 19:01:24 -0500

    things

---
+++ app/src/main/java/com/example/a2019scouting/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2019scouting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.morrie.frc2018scout.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
